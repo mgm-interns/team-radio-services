66a8a27a0d7248d78027bca05207b3e20f2db204
Dat Nguyen <datvictory2341@gmail.com>
2018-03-21 16:33:22 +0700

    remove config local.properties

--- src/main/java/com/mgmtp/radio/config/RadioConfig.java
+++ src/main/java/com/mgmtp/radio/config/RadioConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource(value = {"classpath:local.properties"}, ignoreResourceNotFound = true)
 public class RadioConfig {
 
 }
