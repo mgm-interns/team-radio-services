986ee38e0e8cfdb22aaf7d6602ff9d86b2c9ab61
Tran-Hoang-Lam <Lam.Hoang.Tran@mgm-tp.com>
2018-03-27 00:28:56 +0700

    Add service

--- src/main/java/com/mgmtp/radio/service/station/StationService.java
+++ src/main/java/com/mgmtp/radio/service/station/StationService.java
@@ -1,4 +1,8 @@
 package com.mgmtp.radio.service.station;
 
+import com.mgmtp.radio.domain.station.Station;
+import reactor.core.publisher.Mono;
+
 public interface StationService {
+    Mono<Station> findStationByIdAndDeletedFalse(String stationId);
 }
