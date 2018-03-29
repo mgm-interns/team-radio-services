3f1e65b613b09a516da9799b379b5a35359b7c77
MGM\dvnguyen <dungnv.itdn@gmail.com>
2018-03-29 16:36:31 +0700

    Add domain for chat

---
+++ src/main/java/com/mgmtp/radio/domain/chat/User.java
@@ -0,0 +1,11 @@
+package com.mgmtp.radio.domain.chat;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class User {
+    private String username;
+    private String avatar;
+}
