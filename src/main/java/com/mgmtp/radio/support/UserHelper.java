22e31811e9ec00cbe638e212634b3d6a04ecad7a
MGM\dvnguyen <dungnv.itdn@gmail.com>
2018-04-05 16:23:55 +0700

    remove validator
    (cherry picked from commit 9bfcd7b)

--- src/main/java/com/mgmtp/radio/support/UserHelper.java
+++ src/main/java/com/mgmtp/radio/support/UserHelper.java
@@ -1,5 +1,6 @@
 package com.mgmtp.radio.support;
 
+import com.mgmtp.radio.domain.conversation.FromUser;
 import com.mgmtp.radio.domain.user.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -16,9 +17,17 @@ public class UserHelper {
     public Optional<User> getCurrentUser() {
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         Optional<User> user = Optional.empty();
-        if(!principal.equals(userType)) {
+        if (!principal.equals(userType)) {
             user = Optional.of((User) principal);
         }
         return user;
+    }
+
+    public FromUser convertUserToFromUser(User user) {
+        FromUser fromUser = new FromUser();
+        fromUser.setId(user.getId());
+        fromUser.setUsername(user.getUsername());
+        fromUser.setAvatarUrl(user.getAvatarUrl());
+        return fromUser;
     }
 }
