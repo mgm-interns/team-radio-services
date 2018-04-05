b91452dfaf6d3a22b13ba87be0af142a23bbba75
MGM\dvnguyen <dungnv.itdn@gmail.com>
2018-04-05 14:15:11 +0700

    Update user according to optional
    (cherry picked from commit 2c52d62)

--- src/main/java/com/mgmtp/radio/controller/v1/MessageController.java
+++ src/main/java/com/mgmtp/radio/controller/v1/MessageController.java
@@ -2,14 +2,17 @@ package com.mgmtp.radio.controller.v1;
 
 import com.mgmtp.radio.controller.BaseRadioController;
 import com.mgmtp.radio.controller.response.RadioSuccessResponse;
+import com.mgmtp.radio.domain.user.User;
 import com.mgmtp.radio.dto.conversation.MessageDTO;
 import com.mgmtp.radio.exception.RadioBadRequestException;
 import com.mgmtp.radio.service.conversation.MessageService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Log4j2
@@ -27,11 +30,18 @@ public class MessageController extends BaseRadioController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
-    public Mono<RadioSuccessResponse<MessageDTO>> store(@Validated @RequestBody MessageDTO messageDTO, BindingResult bindingResult) {
+    public Mono<RadioSuccessResponse<MessageDTO>> store(@PathVariable(value = "stationId") String stationId, @Validated @RequestBody MessageDTO messageDTO, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return Mono.error(new RadioBadRequestException(bindingResult.getAllErrors().get(0).getDefaultMessage()));
         }
-        return messageService.create(messageDTO).map(RadioSuccessResponse::new);
+        User user = getCurrentUser().get();
+        return messageService.create(stationId, user, messageDTO).map(RadioSuccessResponse::new);
+    }
+
+    @GetMapping(value = "", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public Flux<RadioSuccessResponse<MessageDTO>> getAll(@PathVariable(value = "stationId") String stationId) {
+        return messageService.findByStationId(stationId).map(RadioSuccessResponse::new);
     }
 
 }
