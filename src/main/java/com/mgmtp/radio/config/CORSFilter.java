c847ef9c1ef7053047157500c306cabd9f4d5dc2
Dat Nguyen <datvictory2341@gmail.com>
2018-04-06 16:31:51 +0700

    fix CORS

--- src/main/java/com/mgmtp/radio/config/CORSFilter.java
+++ src/main/java/com/mgmtp/radio/config/CORSFilter.java
@@ -19,7 +19,7 @@ public class CORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with,Authorization");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with,Authorization, Content-Type");
         chain.doFilter(request, response);
     }
 
