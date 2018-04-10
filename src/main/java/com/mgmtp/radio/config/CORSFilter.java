43a12ba83ad13cc29781f8882dd81457dd1195fc
Dat Nguyen <datvictory2341@gmail.com>
2018-04-10 14:27:18 +0700

    add PATCH to CORS config

--- src/main/java/com/mgmtp/radio/config/CORSFilter.java
+++ src/main/java/com/mgmtp/radio/config/CORSFilter.java
@@ -17,7 +17,7 @@ public class CORSFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE, PATCH");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with,Authorization, Content-Type");
         chain.doFilter(request, response);
