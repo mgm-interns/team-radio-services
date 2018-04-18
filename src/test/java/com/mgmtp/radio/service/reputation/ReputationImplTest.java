3d4daca3e1f11942fd2735f2c51a35c10b7fe89e
NguyenAnhQuoc <quocnguyen.it.acoustic1994@gmail.com>
2018-04-18 15:52:41 +0700

    remove redundant import

--- src/test/java/com/mgmtp/radio/service/reputation/ReputationImplTest.java
+++ src/test/java/com/mgmtp/radio/service/reputation/ReputationImplTest.java
@@ -3,7 +3,6 @@ package com.mgmtp.radio.service.reputation;
 import com.mgmtp.radio.RadioApplicationTests;
 import com.mgmtp.radio.domain.reputation.Reputation;
 import com.mgmtp.radio.dto.reputation.ReputationDTO;
-import com.mgmtp.radio.dto.user.UserDTO;
 import com.mgmtp.radio.exception.RadioNotFoundException;
 import com.mgmtp.radio.mapper.reputation.ReputationMapper;
 import com.mgmtp.radio.respository.reputation.ReputationRepository;
@@ -17,7 +16,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import reactor.core.publisher.Mono;
 
 import static org.junit.Assert.*;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
@@ -65,5 +63,4 @@ public class ReputationImplTest {
         assertEquals(reputationDTO.getScore(), result.getScore());
         assertEquals(reputationDTO.isUpdateAvatarAlready(), result.isUpdateAvatarAlready());
     }
-
 }
