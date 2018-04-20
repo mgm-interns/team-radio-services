910c735c9f330842baddda5651c27dccfce5e0f9
Superman <bao.mmo3@gmail.com>
2018-04-20 14:20:41 +0700

    Fix get history api (#46)
    * fix add song
    
    * fix get history api

--- src/main/java/com/mgmtp/radio/domain/station/Station.java
+++ src/main/java/com/mgmtp/radio/domain/station/Station.java
@@ -29,7 +29,7 @@ public class Station {
 
 	private String ownerId;
 
-	private List<String> playlist = Collections.emptyList();
+	private List<String> playlist;
 
 	@JsonDeserialize(using = LocalDateDeserializer.class)
 	@JsonSerialize(using = LocalDateSerializer.class)
