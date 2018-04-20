975c873ed1668ed78055858f80ea428d038e58d4
Tran-Hoang-Lam <lam.hoang.tran@mgm-tp.com>
2018-04-20 22:05:45 +0700

    fix sort

--- src/main/java/com/mgmtp/radio/service/station/SongServiceImpl.java
+++ src/main/java/com/mgmtp/radio/service/station/SongServiceImpl.java
@@ -178,8 +178,12 @@ public class SongServiceImpl implements SongService {
     }
 
     private Comparator<SongDTO> sortByVote = (SongDTO song1, SongDTO song2) -> {
-        if (song1.getStatus() == SongStatus.playing || song2.getStatus() == SongStatus.playing){
+        if (song1.getStatus() == SongStatus.playing){
             return -1;
+        }
+
+        if (song2.getStatus() == SongStatus.playing){
+            return 1;
         }
 
         int song1Vote = song1.getUpVoteCount() - song1.getDownVoteCount();
