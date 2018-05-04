c89f8ea33392d7aae7afaa10bb9bba5feda11d80
Dat Nguyen <datvictory2341@gmail.com>
2018-05-04 11:50:03 +0700

    clean unused files

--- src/main/java/com/mgmtp/radio/respository/reputation/ReputationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mgmtp.radio.respository.reputation;
-
-import com.mgmtp.radio.domain.reputation.Reputation;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import reactor.core.publisher.Mono;
-
-public interface ReputationRepository extends ReactiveMongoRepository<Reputation, String> {
-    Mono<Reputation> findByUserId(String userId);
-}
