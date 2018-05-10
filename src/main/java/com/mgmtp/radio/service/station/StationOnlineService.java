49dc3566719a9e3b1a672bf80f9596b2e4d9c906
Hy <hy.duc.nguyen@mgm-tp.com>
2018-05-10 14:58:53 +0700

    Fix bug cannot create station - Update cache station list after update station

--- src/main/java/com/mgmtp/radio/service/station/StationOnlineService.java
+++ src/main/java/com/mgmtp/radio/service/station/StationOnlineService.java
@@ -6,6 +6,12 @@ import com.mgmtp.radio.dto.user.UserDTO;
 import java.util.Map;
 
 public interface StationOnlineService {
+    /**
+     * Add a stationDTO to list all stations
+     * - If the stationId is existed, then the old station in list would be replaced by the new station
+     * - Else, add a new station to list
+     * @param stationDTO
+     */
     void addStationToList(StationDTO stationDTO);
 
     void removeStationFromList(String stationId);
